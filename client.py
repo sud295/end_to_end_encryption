990f93ada2ccb0e90fc289573e8670b9e7d9b53a
sud295 <sonicsid12@gmail.com>
2023-08-09 10:49:14 -0400

    basic messaging

--- client.py
+++ client.py
@@ -1,8 +1,8 @@
 import socket
 import threading
 
-HOST = "127.0.0.1"
-PORT = 1234
+HOST = "127.0.0.1"  
+PORT = 1234 
 
 def send_thread(sockfd: socket.socket):
     while True:
@@ -13,19 +13,16 @@ def recv_thread(sockfd: socket.socket):
     while True:
         recv_message = sockfd.recv(1024)
         if not recv_message:
-            print("Client Disconnected")
-            break
+                print("Server Disconnected")
+                break
         print(recv_message.decode())
 
 def main():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as fd:
-        fd.bind((HOST, PORT))
-        fd.listen()
-        client_fd, client_addr = fd.accept()
-        print(f"Connected by {client_addr}")
-        s_thr = threading.Thread(target=send_thread,args=(client_fd,))
-        r_thr = threading.Thread(target=recv_thread,args=(client_fd,))
-
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_fd:
+        server_fd.connect((HOST, PORT))
+        s_thr = threading.Thread(target=send_thread,args=(server_fd,))
+        r_thr = threading.Thread(target=recv_thread,args=(server_fd,))
+        
         s_thr.start()
         r_thr.start()
 
